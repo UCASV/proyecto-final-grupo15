a89ca23d10e13ce946213d5e200c5eb2a228b85c
majocampos92 <maria_campos84@hotmail.com>
2021-06-25 02:26:54 -0600

    Adding view to branch Users

---
+++ Proyecto-POO/Proyecto-POO/View/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Proyecto_POO.MySQLContext;
+
+namespace Proyecto_POO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // usuario: txtUser, contraseña: txtPassword, olvidar contraseña: lblForgotPassword
+
+            var db = new ProyectoContext();
+            string user = txtUser.Text;
+
+            string password = txtPassword.Text;
+
+            List<Manager> managers = db.Managers
+                .ToList();
+
+            List<Manager> result = managers
+                .Where(u => u.Username == user && u.ManagerPassword == password)
+                .ToList();
+
+            if (result.Count() > 0)
+            {
+                MessageBox.Show("Bienvenido!", "",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                frmMenu frm = new frmMenu(result[0]);
+                frm.Show();
+                this.Hide();
+            }
+            // sino, mostrar un mensaje de error
+            else
+                MessageBox.Show("ERROR!", "",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void lblForgotPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void lblForgotPassword_Click(object sender, EventArgs e)
+        {
+            frmChangePassword frm = new frmChangePassword();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Proyecto-POO/Proyecto-POO/View/frmMenu.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Proyecto_POO.MySQLContext;
+
+namespace Proyecto_POO
+{
+    public partial class frmMenu : Form
+    {
+        private Manager IdManager { get; set; }
+        private Booth IdBooth { get; set; }
+
+        public frmMenu(Manager? IdManager)
+        {
+            InitializeComponent();
+            this.IdManager = IdManager;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            frmRegister frm = new frmRegister();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            frmPreCheck frm = new frmPreCheck();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            frmVaccination frm = new frmVaccination();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            frmNewAppointment frm = new frmNewAppointment();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void frmMenu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Form1 frm = new Form1();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void frmMenu_Load(object sender, EventArgs e)
+        {
+            var db = new ProyectoContext();
+            var M = db.Managers.ToList();
+            var B = db.Booths.ToList();
+
+            var innerjoin =
+                from booths in B 
+                join managers in M on booths.IdManager equals managers.IdManager
+                select new { IdB = booths.IdBooth};
+
+            var hourNow = DateTime.Now.ToString("HH:mm:ss");
+            var dateNow = DateTime.Now.ToString("yyyy-MM-dd");
+            lblFecha.Text = "" + dateNow;
+            lblHora.Text = "" + hourNow;
+            labelCabina.Text = "" + innerjoin.FirstOrDefault().IdB;
+
+            string RecordDate = lblFecha.Text;
+            string RecordHour = lblHora.Text;
+            int IdBooth =  Convert.ToInt32(labelCabina.Text);
+
+            Record r = new();
+            r.IdBooth = IdBooth;
+            r.IdManager = this.IdManager.IdManager;
+            r.RecordDate = DateTime.Parse(RecordDate);
+            r.RecordHour = TimeSpan.Parse(RecordHour);
+            db.Add(r);
+            db.SaveChanges();
+            MessageBox.Show("insertado!", "",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
